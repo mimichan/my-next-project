f1a592df3a7cf51eb700ab716a65f081603e33a8
k.koizumi <koizumi@nttcoms.com>
2024-09-25 03:11:55 +0900

    デプロイエラー解消
    getCategoryDetail

--- app/news/category/[id]/p/[current]/page.tsx
+++ app/news/category/[id]/p/[current]/page.tsx
@@ -1,5 +1,5 @@
 import { notFound } from "next/navigation";
-import { getNewsList } from "@/app/_libs/microcms";
+import { getCategoryDetail,getNewsList } from "@/app/_libs/microcms";
 import NewsList from "@/app/_components/NewsList";
 import Pagenation from "@/app/_components/Pagenation";
 
